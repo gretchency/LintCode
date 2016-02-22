c33e31c4d95a5e36b1f71e2fa7586d123df26d3a
gretchency <gretchency7@gmail.com>
2016-02-22 00:18:13 -0500

    added nine chapter

--- LinkedList/ Insert Node in Sorted Linked List.java
+++ LinkedList/ Insert Node in Sorted Linked List.java
@@ -31,10 +31,33 @@ public class Solution {
         while (cur.next != null && cur.next.val <= val) {
             cur = cur.next;
         }
-        ListNode next = cur.next;
+        node.next = cur.next;
         cur.next = node;
-        node.next = next;
         }
         return head;
+    }  
+}
+
+public class Solution {
+    /**
+     * @param head: The head of linked list.
+     * @param val: an integer
+     * @return: The head of new linked list
+     */
+    public ListNode insertNode(ListNode head, int val) { 
+        ListNode node = new ListNode(val);
+        ListNode dummy = new ListNode(0);
+        //nine chapter: use a dummy before head, so that without considering when head.val > node.val
+        dummy.next = head;
+        head = dummy;
+        
+        // find the last element <= val
+        while (head.next != null && head.next.val <= val) {
+            head = head.next;
+        }
+        node.next = head.next;
+        head.next = node;
+        
+        return dummy.next;
     }  
 }
