639a048063e9026ec713a8a0940d3474153cbe58
gretchency <gretchency7@gmail.com>
2016-02-22 00:26:19 -0500

    use dummy to remove element

---
+++ LinkedList/ Remove Linked List Element.java
@@ -0,0 +1,33 @@
+//Remove all elements from a linked list of integers that have value val.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param head a ListNode
+     * @param val an integer
+     * @return a ListNode
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        // use dummy so that head can be removed as well
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        
+        while (head.next != null) {
+            if (head.next.val == val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        
+        return dummy.next;
+    }
+}
